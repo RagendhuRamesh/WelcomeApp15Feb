65c7602274dc5de14b2f501dc268033bb784f01b
RagendhuRamesh <ragendhuram@gmail.com>
2024-02-24 03:14:55 +0530

    Add project files.

---
+++ WelcomeApp15Feb/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using WelcomeApp15;
+
+Console.WriteLine("Welcome to Auhtentication  App");
+SiginIn obj = new SiginIn();
+Console.WriteLine(obj.Auhtenticate("admin", "admin@1256"));
---
+++ WelcomeApp15Feb/SignIn.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WelcomeApp15
+{
+    internal class SiginIn
+    {
+        internal string Auhtenticate(string userName, string password)
+        {
+            string msg;
+            if (string.IsNullOrEmpty(userName) || string.IsNullOrEmpty(password))
+            {
+                msg = "User name and password required";
+            }
+            else
+            {
+                if ((userName == "admin") || (password == "admin@1256"))
+                {
+                    msg = "Authentication Pass";
+                }
+                else
+                {
+                    msg = "Authentication Fail";
+                }
+            }
+            return msg;
+        }
+    }
+}
